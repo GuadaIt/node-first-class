ff9ab15df263126f115c2ce4c01eb945d484713f
GuadaIt <guada.iturralde@gmail.com>
2020-08-29 15:37:37 -0400

    get query params with url module

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/peliculas') {
+
+  const { query, pathname } = url.parse(req.url, true);
+
+  if (pathname === '/peliculas') {
+    if (query.id === "1") {
+      fs.readFile('./assets/singleMovie.json', 'utf-8', (err, data) => {
+        res.end(data)
+      })
+    }
     fs.readFile('./assets/movies.json', 'utf-8', (err, data) => {
       res.end(data);
     });
-  } else if (req.url === '/series') {
+  } else if (pathname === '/series') {
     fs.readFile('./assets/series.json', 'utf-8', (err, data) => {
       res.end(data);
     });
